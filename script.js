4512bfaaffc227ab22e62a5d925e48f8d08d687c
Basudev-Pokharel <basudevpokharelfin@gmail.com>
2025-03-19 14:57:56 +0200

    done everything with hero-section now

--- script.js
+++ script.js
@@ -2,8 +2,40 @@
 let hamMenu = document.getElementById("menu");
 let hamMenuBtnOpen = document.getElementById("menu-open");
 function hamshow() {
-  //   hamMenu.style.display = "block";
   hamMenu.classList.toggle("viewed-menu");
 }
 hamMenuBtnOpen.addEventListener("click", hamshow);
-// hamMenu.style.left = "600%";
+
+//Typing effect begins from here
+let typeBox = document.getElementById("typeBox");
+let typeList = ["Developer", "Designer", "Student", "Tech Enthusiast"];
+typingEffect(typeList, typeBox);
+function typingEffect(typingList, element) {
+  //Declaring function to use in another place as well
+  let wordIndex = 0;
+  let typeIndex = 0;
+  let goReverse = false;
+  setInterval(() => {
+    if (!goReverse) {
+      element.textContent += typingList[typeIndex][wordIndex];
+      wordIndex++;
+    } else {
+      element.innerText = element.innerText.slice(
+        0,
+        element.innerText.length - 1
+      );
+    }
+
+    if (wordIndex == typeList[typeIndex].length) {
+      goReverse = true;
+    }
+    if (element.innerText == "") {
+      goReverse = false;
+      typeIndex++;
+      wordIndex = 0;
+    }
+    if (typeIndex == typeList.length) {
+      typeIndex = 0;
+    }
+  }, 200);
+}
